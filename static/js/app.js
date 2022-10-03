5f0507d671617c391941a8b752b199889116b7d4
Rafael Lewis <rafaelewis22@gmail.com>
2022-10-02 23:04:19 -0500

    added bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,47 @@
 console.log('This is app.js');
 
 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+
 function DrawBargraph(sampleId)
 {
     console.log('DrawBargraph(${sampleId})');
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId); 
+        let result = resultArray[0]
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0, 10).map(otuId => 'OTU ${otuId}').reverse();
+
+        //Create a trace object
+        let barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        }
+
+        //Put the trace object into an array
+        let barArray = [barData];
+
+        // Create a layout object
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found"
+        }
+        //Call the Plotly function
+        Plotly.newPlot("bar", barArray, barLayout);
+
+
+    });
 }
 
 function DrawBubblechart(sampleId)
@@ -37,8 +75,6 @@ function InitDashboard()
 
     // Get a handle to the dropdown
     let selector =d3.select("#selDataset");
-
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
     d3.json(url).then(data => {
         console.log("Here's the data:", data);
