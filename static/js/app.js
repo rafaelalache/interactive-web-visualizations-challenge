5aebad6752c2449c19d5e42be93efa75b95abe6e
Rafael Lewis <rafaelewis22@gmail.com>
2022-10-02 23:22:25 -0500

    tried to add bubbles

--- static/js/app.js
+++ static/js/app.js
@@ -47,6 +47,44 @@ function DrawBargraph(sampleId)
 function DrawBubblechart(sampleId)
 {
     console.log('DrawBubblechart(${sampleId})');
+
+    d3.json(url).then(data => {
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels
+        let sample_values = result.sample_values;
+
+        //Create a trace
+        let bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                // colorscale: "Earth"
+            }
+        };
+        //Put the trace into an array
+        let bubbleArray = [bubbleData];
+
+        //Create a layout object
+        let bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: {t: 30},
+            hovermode: "closest",
+            xaxis: { title: "OTU ID"}
+        };
+        //Call Plotly
+        Plotly.newPlot("bubble".bubbleArray, bubbleLayout);
+    })
+
+
+
+
 }
 
 function DrawGauge(sampleId)
